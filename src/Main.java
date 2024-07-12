b076e8ba02dbb6fde7a4e0fae9ee731673fde1ae
matic <maticpozar054@gmail.com>
2024-07-12 13:24:57 +0200

    adde traversal func

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Main {
     static int width;
@@ -14,6 +16,7 @@ public class Main {
     static int direction = 1;
 
     static boolean [][] explored;
+    static int [][] costs;
 
     static int score = 0;
     static boolean lose = false;
@@ -21,7 +24,10 @@ public class Main {
 
     static int x,y;
     static int goalX,goalY;
+
+    static ArrayList<String> trace = new ArrayList<>();
     public static void main(String[] args) {
+        ArrayList<String> t = new ArrayList<>();
         String filePath = "wumpus_world.txt";
         BufferedReader reader = null;
         try {
@@ -39,6 +45,7 @@ public class Main {
                     P = new String[width][height];
                     KB = new String[width][height][2];
                     explored = new boolean[width][height];
+                    costs = new int[width][height];
                 } else {
                     data.add(line);
                     if (line.charAt(0) == 'A'){
@@ -52,7 +59,10 @@ public class Main {
                 }
             }
             for (int i = 0; i < width; i++)
-                for (int j = 0; j < height; j++) explored[i][j] = false;
+                for (int j = 0; j < height; j++) {
+                    explored[i][j] = false;
+                    costs[i][j] = Math.abs(i-goalX)+Math.abs(j-goalY);
+                }
             explored[x][y] = true;
             for (String s: data) {
                 if (s.length() < 4){
@@ -85,51 +95,280 @@ public class Main {
         Tell(x,y);
     }
 
-    static void makeMove(){
-        if (x < goalX){
-            String ask = ASK(x+1,y);
-            switch (ask){
+    public void traversal(){
+        if (x == goalX && y == goalY) return;
+        int costRight = Integer.MAX_VALUE;
+        int costLeft = Integer.MAX_VALUE;
+        int costUp = Integer.MAX_VALUE;
+        int costDown = Integer.MAX_VALUE;
+        if (x-1 > 0) {
+            switch (ASK(x-1,y)){
                 case "yes":
+                    costLeft = costs[x-1][y];
+                    if (explored[x-1][y]) costLeft = Integer.MAX_VALUE;
                     break;
                 case "no":
                     break;
                 case "uncertian":
-                    break;
-            }
-        } else if (x > goalX){
-            String ask = ASK(x-1,y);
-            switch (ask){
-                case "yes":
-                    break;
-                case "no":
-                    break;
-                case "uncertian":
-                    break;
-            }
-        } else if (y < goalY){
-            String ask = ASK(x,y+1);
-            switch (ask){
-                case "yes":
-                    break;
-                case "no":
-                    break;
-                case "uncertian":
-                    break;
-            }
-        } else if (y > goalY) {
-            String ask = ASK(x,y-1);
-            switch (ask){
-                case "yes":
-                    break;
-                case "no":
-                    break;
-                case "uncertian":
+                    costLeft = costLeft/2;
                     break;
             }
         }
+        if (x+1 < width) {
+            switch (ASK(x+1,y)){
+                case "yes":
+                    costRight = costs[x+1][y];
+                    if (explored[x+1][y]) costRight = Integer.MAX_VALUE;
+                    break;
+                case "no":
+                    break;
+                case "uncertian":
+                    costRight = costRight/2;
+                    break;
+            }
+        }
+        if (y-1 > 0) {
+            switch (ASK(x,y-1)){
+                case "yes":
+                    costDown = costs[x][y-1];
+                    if (explored[x][y-1]) costDown = Integer.MAX_VALUE;
+                    break;
+                case "no":
+                    break;
+                case "uncertian":
+                    costDown = costDown/2;
+                    break;
+            }
+        }
+        if (y+1 < height) {
+            switch (ASK(x,y+1)){
+                case "yes":
+                    costUp = costs[x][y+1];
+                    if (explored[x][y+1]) costUp = Integer.MAX_VALUE;
+                    break;
+                case "no":
+                    break;
+                case "uncertian":
+                    costUp = costUp/2;
+                    break;
+            }
+        }
+        if (costRight == costLeft &&
+                costLeft == costDown &&
+                costDown == costUp &&
+                (costRight == Integer.MAX_VALUE  || costRight == Integer.MAX_VALUE/2)){
+            ArrayList<String> backtrack = new ArrayList<>(trace);
+            Collections.reverse(backtrack);
+            while (true) {
+                actuator(backtrack.remove(0));
+                int cRight = Integer.MAX_VALUE;
+                int cLeft = Integer.MAX_VALUE;
+                int cUp = Integer.MAX_VALUE;
+                int cDown = Integer.MAX_VALUE;
+                if (x-1 > 0) {
+                    switch (ASK(x-1,y)){
+                        case "yes":
+                            cLeft = costs[x-1][y];
+                            if (explored[x-1][y]) cLeft = Integer.MAX_VALUE;
+                            break;
+                        case "no":
+                            break;
+                        case "uncertian":
+                            cLeft = cLeft/2;
+                            break;
+                    }
+                }
+                if (x+1 < width) {
+                    switch (ASK(x+1,y)){
+                        case "yes":
+                            cRight = costs[x+1][y];
+                            if (explored[x+1][y]) cRight = Integer.MAX_VALUE;
+                            break;
+                        case "no":
+                            break;
+                        case "uncertian":
+                            cRight = cRight/2;
+                            break;
+                    }
+                }
+                if (y-1 > 0) {
+                    switch (ASK(x,y-1)){
+                        case "yes":
+                            cDown = costs[x][y-1];
+                            if (explored[x][y-1]) cDown = Integer.MAX_VALUE;
+                            break;
+                        case "no":
+                            break;
+                        case "uncertian":
+                            cDown = cDown/2;
+                            break;
+                    }
+                }
+                if (y+1 < height) {
+                    switch (ASK(x,y+1)){
+                        case "yes":
+                            cUp = costs[x][y+1];
+                            if (explored[x][y+1]) cUp = Integer.MAX_VALUE;
+                            break;
+                        case "no":
+                            break;
+                        case "uncertian":
+                            cUp = cUp/2;
+                            break;
+                    }
+                }
+                if (!(cRight == cLeft &&
+                        cLeft == cDown &&
+                        cDown == cUp &&
+                        (cRight == Integer.MAX_VALUE  || cRight == Integer.MAX_VALUE/2))) {
+                    String action;
+                    if (cRight <= cLeft && cRight <= cUp && cRight <= cDown) action = "Right";
+                    else if (cLeft <= cRight && cLeft <= cUp && cLeft <= cDown) action = "Left";
+                    else if (cUp <= cRight && cUp <= cLeft && cUp <= cDown) action = "Up";
+                    else action = "Down";
+                    switch (action){
+                        case "Right":
+                            if (direction == 1){
+                                actuator("Forward");
+                            } else if (direction == 2){
+                                actuator("RightTurn");
+                                actuator("Forward");
+                            } else if (direction == -1){
+                                actuator("RightTurn");
+                                actuator("RightTurn");
+                                actuator("Forward");
+                            } else {
+                                actuator("LeftTurn");
+                                actuator("Forward");
+                            }
+                            break;
+                        case "Left":
+                            if (direction == 1){
+                                actuator("RightTurn");
+                                actuator("RightTurn");
+                                actuator("Forward");
+                            } else if (direction == 2){
+                                actuator("LeftTurn");
+                                actuator("Forward");
+                            } else if (direction == -1){
+                                actuator("Forward");
+                            } else {
+                                actuator("RightTurn");
+                                actuator("Forward");
+                            }
+                            break;
+                        case "up":
+                            if (direction == 1){
+                                actuator("LeftTurn");
+                                actuator("Forward");
+                            } else if (direction == 2){
+                                actuator("Forward");
+                            } else if (direction == -1){
+                                actuator("RightTurn");
+                                actuator("Forward");
+                            } else {
+                                actuator("RightTurn");
+                                actuator("RightTurn");
+                                actuator("Forward");
+                            }
+                            break;
+                        case "Down":
+                            if (direction == 1){
+                                actuator("RightTurn");
+                                actuator("Forward");
+                            } else if (direction == 2){
+                                actuator("RightTurn");
+                                actuator("RightTurn");
+                                actuator("Forward");
+                            } else if (direction == -1){
+                                actuator("LeftTurn");
+                                actuator("Forward");
+                            } else {
+                                actuator("Forward");
+                            }
+                            break;
+                    }
+                    break;
+                }
+                if (backtrack.size() == 0) {
+                    System.out.println("Unsolvable");
+                    return;
+                }
+            }
+        } else {
+            String action;
+            if (costRight <= costLeft && costRight <= costUp && costRight <= costDown) action = "Right";
+            else if (costLeft <= costRight && costLeft <= costUp && costLeft <= costDown) action = "Left";
+            else if (costUp <= costRight && costUp <= costLeft && costUp <= costDown) action = "Up";
+            else action = "Down";
+            switch (action){
+                case "Right":
+                    if (direction == 1){
+                        actuator("Forward");
+                    } else if (direction == 2){
+                        actuator("RightTurn");
+                        actuator("Forward");
+                    } else if (direction == -1){
+                        actuator("RightTurn");
+                        actuator("RightTurn");
+                        actuator("Forward");
+                    } else {
+                        actuator("LeftTurn");
+                        actuator("Forward");
+                    }
+                    break;
+                case "Left":
+                    if (direction == 1){
+                        actuator("RightTurn");
+                        actuator("RightTurn");
+                        actuator("Forward");
+                    } else if (direction == 2){
+                        actuator("LeftTurn");
+                        actuator("Forward");
+                    } else if (direction == -1){
+                        actuator("Forward");
+                    } else {
+                        actuator("RightTurn");
+                        actuator("Forward");
+                    }
+                    break;
+                case "up":
+                    if (direction == 1){
+                        actuator("LeftTurn");
+                        actuator("Forward");
+                    } else if (direction == 2){
+                        actuator("Forward");
+                    } else if (direction == -1){
+                        actuator("RightTurn");
+                        actuator("Forward");
+                    } else {
+                        actuator("RightTurn");
+                        actuator("RightTurn");
+                        actuator("Forward");
+                    }
+                    break;
+                case "Down":
+                    if (direction == 1){
+                        actuator("RightTurn");
+                        actuator("Forward");
+                    } else if (direction == 2){
+                        actuator("RightTurn");
+                        actuator("RightTurn");
+                        actuator("Forward");
+                    } else if (direction == -1){
+                        actuator("LeftTurn");
+                        actuator("Forward");
+                    } else {
+                        actuator("Forward");
+                    }
+                    break;
+            }
+        }
+        traversal();
     }
 
     static void actuator(String action){
+        trace.add(action);
         switch (action){
             case "LeftTurn":
                 if (direction==1) direction = 2;
@@ -169,8 +408,10 @@ public class Main {
                         return;
                     }
                 }
-                explored[x][y] = true;
-                Tell(x,y);
+                if (!explored[x][y]){
+                    explored[x][y] = true;
+                    Tell(x,y);
+                }
                 score--;
                 if (map[x][y] == null){
                     W[x][y] = "no";
@@ -179,10 +420,14 @@ public class Main {
                     score -= 1000;
                     lose = true;
                 } else if (map[x][y].contains("Y")){
-                    score += 1000;
+                    actuator("Grab");
                 }
                 break;
             case "Grab":
+                if (map[x][y].contains("Y")){
+                    score += 1000;
+                    map[x][y].replace(String.valueOf('Y'), "");
+                }
                 break;
             case "Release":
                 break;
@@ -245,7 +490,10 @@ public class Main {
                             pitY = j+1;
                         }
                     }
-                    if (pitCount==1) P[pitX][pitY] = "yes";
+                    if (pitCount==1){
+                        P[pitX][pitY] = "yes";
+                        costs[pitX][pitY] = Integer.MAX_VALUE;
+                    }
 
                     if (KB[i][j][0]=="Smelly") {
                         int wCount = 0;
@@ -286,7 +534,10 @@ public class Main {
                                 wY = j + 1;
                             }
                         }
-                        if (wCount == 1) W[pitX][pitY] = "yes";
+                        if (wCount == 1){
+                            W[wX][wY] = "yes";
+                            costs[wX][wY] = Integer.MAX_VALUE;
+                        }
                     }
                 }
             }
